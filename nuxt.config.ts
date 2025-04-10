d2aeeb7f49c42642a5c00f5ed30016ca9552f499
david <ramosdavu2006@gmail.com>
2025-04-10 17:05:17 +0200

    Primer commit: agregar la página web

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  nitro: {
+    preset: 'static'
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss']
